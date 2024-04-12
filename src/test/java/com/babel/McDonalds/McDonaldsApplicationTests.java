35e8f58d43cfd2d1a267363fd29864e06798f4e8
AlbertoF17 <albertofergom@gmail.com>
2024-04-12 10:22:19 +0200

    McDonalds SprinBoot App

---
+++ src/test/java/com/babel/McDonalds/McDonaldsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babel.McDonalds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McDonaldsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
