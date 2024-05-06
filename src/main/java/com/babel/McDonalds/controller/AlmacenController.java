20c305f28d94d94af372325d6fa3e5088dd440e9
AlbertoF17 <albertofergom@gmail.com>
2024-05-06 11:37:46 +0200

    Swagger y Jakarta de mi controlador

--- src/main/java/com/babel/McDonalds/controller/AlmacenController.java
+++ src/main/java/com/babel/McDonalds/controller/AlmacenController.java
@@ -4,6 +4,10 @@ package com.babel.McDonalds.controller;
 import com.babel.McDonalds.model.Producto;
 import com.babel.McDonalds.service.IAlmacenService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -11,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/store")
+@Tag(name = "AlmacenController", description = "Controlador que posee los endpoints relacionados con las operaciones del almacén")
 public class AlmacenController {
 
     private final IAlmacenService almacenService;
@@ -26,20 +31,35 @@ public class AlmacenController {
     }
 
     @PostMapping(value = "/product/{idProducto}")
-    @Operation(summary = "Muestra el producto según el ID y su stock en el almacén")
+    @Operation(summary = "Muestra el producto según el ID y su stock en el almacén",
+            responses = {
+                    @ApiResponse(description = "Producto",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = Producto.class))),
+                    @ApiResponse(responseCode = "400", description = "No existe el producto con este ID")})
     public HashMap<String, Integer> obtenerCantidadProducto(@PathVariable int idProducto) {
         return almacenService.obtenerNombreYCantidadProducto(idProducto);
     }
 
     @PostMapping(value = "/push/{idProducto}")
-    @Operation(summary = "Añade una unidad al stock del producto según su ID al almacén")
+    @Operation(summary = "Añade una unidad al stock del producto según su ID al almacén",
+            responses = {
+                    @ApiResponse(description = "Producto",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = Producto.class))),
+                    @ApiResponse(responseCode = "400", description = "No existe el producto con este ID")})
     public int pushProducto(@PathVariable int idProducto) {
         almacenService.pushProducto(idProducto);
         return 1;
     }
 
     @PostMapping(value = "/pop/{idProducto}")
-    @Operation(summary = "Resta una unidad al stock del producto según su ID al almacén")
+    @Operation(summary = "Resta una unidad al stock del producto según su ID al almacén",
+            responses = {
+                    @ApiResponse(description = "Producto",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = Producto.class))),
+                    @ApiResponse(responseCode = "400", description = "No existe el producto con este ID")})
     public int popProducto(@PathVariable int idProducto) {
         almacenService.popProducto(idProducto);
         return 1;
