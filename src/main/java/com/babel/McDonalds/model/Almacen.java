4eb2710e322e9e452b45580103de0abac4426924
AlbertoF17 <albertofergom@gmail.com>
2024-05-06 10:23:03 +0200

    Swagger de mi controlador

--- src/main/java/com/babel/McDonalds/model/Almacen.java
+++ src/main/java/com/babel/McDonalds/model/Almacen.java
@@ -1,9 +1,12 @@
 package com.babel.McDonalds.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.HashMap;
 
 public class Almacen {
 
+    @Schema(description = "Mapa con el producto y la cantidad de unidades existentes en el Almacén")
     private HashMap<Producto, Integer> almacen;
 
     public Almacen() {
